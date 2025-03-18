36772556ebd0eeeaaced9cdabf352d420d53829e
Felipe-M-de-Sousa <felipe.sousa.1106@gmail.com>
2025-03-17 23:19:55 -0300

    Aula de java.

---
+++ PrimeiroProjeto/src/br/com/fiap/PrimeiroProjeto.java
@@ -0,0 +1,14 @@
+package br.com.fiap;
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+        // Comentário de uma linha
+        System.out.println("Hello, World!");
+
+        /*
+         * Comentário
+         * de múltiplas
+         * linhas
+         */
+    }
+}
