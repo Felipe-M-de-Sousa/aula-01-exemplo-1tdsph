ee3cd5a532cdb214fafd28fa18cf89fdf629ac30
Felipe-M-de-Sousa <felipe.sousa.1106@gmail.com>
2025-03-20 23:18:06 -0300

    Adicionado aulas da semana.

---
+++ Projeto02/src/br/com/fiap/Constante.java
@@ -0,0 +1,13 @@
+package br.com.fiap;
+
+public class Constante {
+    public static void main(String[] args) {
+        // declaração e inicializacão de constantes
+        final double PI = 3.1415;
+        final float  GRAVIDADE_TERRESTRE = 9.8f;
+
+        // Sequência de Escape: \n (pula de linha)
+        // Sequência de Escape: \t (tabulação)
+        System.out.println("Valor de PI:\t\t" + PI + "\nGravidade da Terra:\t" + GRAVIDADE_TERRESTRE);
+    }
+}
